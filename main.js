65040b3cf84fb2d049a7307913c1d6b198e1a651
Midhun777 <midmat4828@gmail.com>
2024-02-14 00:31:43 +0530

    update

--- main.js
+++ main.js
@@ -2,3 +2,17 @@ function textAlert() {
 	window.alert("This feature is not yet available 😕");
 }
 
+let menuOpen = document.querySelector(".menu");
+let settingsHide = document.querySelector(".settings");
+
+function menuOpenFuncion() {
+	menuOpen.style.display = "block";
+	settingsHide.style.display = "none";
+
+}
+
+function menuCloseFuncion() {
+	menuOpen.style.display = "none";
+	settingsHide.style.display = "block";
+
+}
