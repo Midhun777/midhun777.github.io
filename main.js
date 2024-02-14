630e0dd805e64418f538d9a9b2fc77b0f2549f3d
Midhun777 <midmat4828@gmail.com>
2024-02-14 12:50:35 +0530

    update

--- main.js
+++ main.js
@@ -3,16 +3,15 @@ function textAlert() {
 }
 
 let menuOpen = document.querySelector(".menu");
+let menuOpenCont = document.querySelector(".menu-cont");
 let settingsHide = document.querySelector(".settings");
 
 function menuOpenFuncion() {
 	menuOpen.style.display = "block";
 	settingsHide.style.display = "none";
-
 }
 
 function menuCloseFuncion() {
 	menuOpen.style.display = "none";
 	settingsHide.style.display = "block";
-
 }
