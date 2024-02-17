77de5fc6ee72b112b3c08626d4f620be451d27e9
Midhun777 <midmat4828@gmail.com>
2024-02-17 17:21:46 +0530

    update

--- main.js
+++ main.js
@@ -2,8 +2,16 @@ let menuSrc = document.querySelector(".menu");
 let settingsSrc = document.querySelector(".settings");
 let backgroundSrc = document.querySelector(".body");
 let cardSrc = document.querySelector(".card");
-let aSrc = document.querySelector(".card");
 let icon = document.querySelector("i");
+
+var theme = document.getElementById('theme');
+
+theme.onclick = function() {
+	document.body.classList.toggle("light-theme");
+}
+
+
+
 
 function menuOpenFuncion() {
 	menuSrc.style.display = "block";
@@ -18,7 +26,7 @@ function menuCloseFuncion() {
 function lightMode() {
 	backgroundSrc.style.backgroundColor = "#FAFAFA";
 	backgroundSrc.style.color = "#484B6A";
-	aSrc.style.color = "#484B6A";
+	cardSrc.style.color = "#484B6A";
 	cardSrc.style.backgroundColor = "#E5E5F1";
 	menuSrc.style.backgroundColor = "#E5E5F1E6";
 	icon.style.color = "#484B6A";
